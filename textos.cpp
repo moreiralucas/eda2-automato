1edc00565664b2bc6d5247edd17592615f537dc5
moreiralucas <moreiralucascc@gmail.com>
2018-01-20 21:26:53 -0300

    Implementação da função de computar a tabela

--- textos.cpp
+++ textos.cpp
@@ -8,7 +8,7 @@ Search_word::Search_word(int size, std::string text, std::string pattern){
     this->size = size;
 }
 
-void Search_word::print_datas() {
+void Search_word::print_datas() { // remover esse método
     print "Text: " << this->text << '\n';
     print "Pattern: " << this->pattern << '\n';
     print "Size: " << this->size << '\n';
@@ -17,6 +17,15 @@ void Search_word::search_pattern() {
     // TODO: Implement you code here
 }
 
-void Search_word::compute_table() {
-    // TODO: Implement you code here
+void Search_word::compute_table(std::string pattern, std::string alphabet) {
+    const int m = pattern.size();
+    const int n = alphabet.size();
+    for (int q = 0; q < m; q++) {
+        for (int i = 0; i < n; i++) {
+            int k = min(m+1,q+2);
+            do {
+                k--;
+            } while(suffix(pattern[k], ));
+        }
+    }
 }
--- textos.hpp
+++ textos.hpp
@@ -2,13 +2,13 @@
 #include <string>
 
 class Search_word {
+        int size;
         std::string text;
         std::string pattern;
-        int size;
-        void compute_table();
+        std::vector<std::vector<int> > table;
     public:
         Search_word(int size, std::string text, std::string pattern);
-        std::vector<std::vector<int> > table;
+        void compute_table(std::string pattern, std::string alphabet);
         void print_datas();
         void search_pattern();
 
